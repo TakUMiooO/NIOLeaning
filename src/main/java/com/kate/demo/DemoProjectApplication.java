54c8cdfc1a3058825a1fa411a53597c772f66f2c
asumn <kachen-123@163.com>
2022-05-10 16:01:49 +0800

    统一异常返回

--- src/main/java/com/kate/demo/DemoProjectApplication.java
+++ src/main/java/com/kate/demo/DemoProjectApplication.java
@@ -2,8 +2,10 @@ package com.kate.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan({"com.kate.demo"}) //当一个项目包括多个项目时，需要将不同的域加入这里，进行扫描注册
 public class DemoProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/kate/demo/common/exception/UnifiedExceptionHandler.java
+++ src/main/java/com/kate/demo/common/exception/UnifiedExceptionHandler.java
@@ -1,6 +1,12 @@
 package com.kate.demo.common.exception;
 
+import com.kate.demo.common.result.R;
+import com.kate.demo.common.result.ResponseEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * @className：UnifiedExceptionHandlar
@@ -8,7 +14,24 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  * @createTime：2022/5/10 11:00
  * @author：kate_chen
  */
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice //该注解就是异常的AOP切面  这个注解让所有response都是以responseBody返回
 public class UnifiedExceptionHandler {
+
+
+    //controller所有发生的异常都会走到这里
+    @ExceptionHandler(value = Exception.class)
+    public R handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        return R.error();
+    }
+
+    //为了处理具体的异常信息，并提示具体的异常信息结果
+    //这里有个sql语法错误的，具体错误抛出
+    @ExceptionHandler(value = BadSqlGrammarException.class)
+    public R handleException(BadSqlGrammarException e){
+        log.error(e.getMessage(),e);
+        return R.setResult(ResponseEnum.BAD_SQL_GRAMMAR_ERROR);
+    }
 
 }
