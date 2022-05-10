85cff58fba2fd53fc3c7b1ee5ab4565d6f33c788
asumn <kachen-123@163.com>
2022-05-10 17:32:24 +0800

    统一异常返回

--- src/main/java/com/kate/demo/common/exception/UnifiedExceptionHandler.java
+++ src/main/java/com/kate/demo/common/exception/UnifiedExceptionHandler.java
@@ -3,10 +3,24 @@ package com.kate.demo.common.exception;
 import com.kate.demo.common.result.R;
 import com.kate.demo.common.result.ResponseEnum;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * @className：UnifiedExceptionHandlar
@@ -46,7 +60,33 @@ public class UnifiedExceptionHandler {
         return R.error().message(e.getMessage()).code(e.getCode());
     }
 
-
+    /**
+     * Controller上一层相关异常
+     * 比如前端给的参数就是错误，tomcat直接就报错了
+     * 根本不是后端的问题
+     * 所以需要直接自定义把所有的错误进行细化
+     */
+    @ExceptionHandler({
+            NoHandlerFoundException.class,
+            HttpRequestMethodNotSupportedException.class,
+            HttpMediaTypeNotSupportedException.class,
+            MissingPathVariableException.class,
+            MissingServletRequestParameterException.class,
+            TypeMismatchException.class,
+            HttpMessageNotReadableException.class,
+            HttpMessageNotWritableException.class,
+            MethodArgumentNotValidException.class,
+            HttpMediaTypeNotAcceptableException.class,
+            ServletRequestBindingException.class,
+            ConversionNotSupportedException.class,
+            MissingServletRequestPartException.class,
+            AsyncRequestTimeoutException.class
+    })
+    public R handleServletException(Exception e) {
+        log.error(e.getMessage(), e);
+        //SERVLET_ERROR(-102, "servlet请求异常"),
+        return R.error().message(ResponseEnum.SERVLET_ERROR.getMessage()).code(ResponseEnum.SERVLET_ERROR.getCode());
+    }
 
 
 }
